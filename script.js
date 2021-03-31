0f43dbc6a897acccf4ca289b31d36fc8af77f036
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-03-31 15:57:27 -0700

    add some HTML elements and basic styling

---
+++ script.js
@@ -0,0 +1,9 @@
+//variable declaration (grab DOM elements)
+// when the page is loaded, using moment, append textcontent of the current time
+// to the top of the page (header, h1/h2 tags maybe)
+// add 3 total columns (2/8/2 if 12 grid layout) apply CSS styling
+// add rows per timeblock
+// a function that determines what time it is and compares color properties using an if/else statement( else would be future)
+// statement would look something like (if current = red, else if = green, else = grey)
+// logic gate basically saying that we don't have to define grey, we only have to say that the current time equals red, other times equal gree, and if neither of those are true then turn grey
+// it's a div, text input, and button. on click of the button it will grab the input value from the div
